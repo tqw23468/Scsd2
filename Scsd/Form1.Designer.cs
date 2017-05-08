79811eef0270250d03142f693980a8d3213e022b
tqw <tqw23468@163.com>
2017-05-08 10:27:12 +0800

    生产速度

---
+++ Scsd/Form1.Designer.cs
@@ -0,0 +1,417 @@
+﻿namespace Scsd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "订货量";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 466);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Let\'s go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(338, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "7";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(495, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(303, 614);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(336, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "几天后交付";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "订单一";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "订单二";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "订单三";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "订单四";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "80";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(338, 125);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(108, 125);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "50";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(338, 173);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "2";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(108, 173);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 21);
+            this.textBox6.TabIndex = 12;
+            this.textBox6.Text = "300";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(338, 221);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 21);
+            this.textBox7.TabIndex = 15;
+            this.textBox7.Text = "5";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(108, 221);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 21);
+            this.textBox8.TabIndex = 14;
+            this.textBox8.Text = "10";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(123, 307);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(39, 21);
+            this.textBox9.TabIndex = 16;
+            this.textBox9.Text = "20";
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 310);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "库存";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 284);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "产能上限";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(108, 281);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 21);
+            this.textBox10.TabIndex = 19;
+            this.textBox10.Text = "200";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(221, 41);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 12);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "产品";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C"});
+            this.comboBox1.Location = new System.Drawing.Point(223, 79);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(87, 20);
+            this.comboBox1.TabIndex = 22;
+            this.comboBox1.Text = "A";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C"});
+            this.comboBox2.Location = new System.Drawing.Point(223, 125);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(87, 20);
+            this.comboBox2.TabIndex = 23;
+            this.comboBox2.Text = "A";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C"});
+            this.comboBox3.Location = new System.Drawing.Point(223, 173);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(87, 20);
+            this.comboBox3.TabIndex = 24;
+            this.comboBox3.Text = "A";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C"});
+            this.comboBox4.Location = new System.Drawing.Point(223, 221);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(87, 20);
+            this.comboBox4.TabIndex = 25;
+            this.comboBox4.Text = "A";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(106, 310);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(11, 12);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "A";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(206, 310);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(11, 12);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "B";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(223, 307);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(39, 21);
+            this.textBox11.TabIndex = 27;
+            this.textBox11.Text = "20";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(308, 310);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(11, 12);
+            this.label12.TabIndex = 30;
+            this.label12.Text = "C";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(325, 307);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(39, 21);
+            this.textBox12.TabIndex = 29;
+            this.textBox12.Text = "20";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 616);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "生产速度与订单的关系";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox12;
+    }
+}
+
---
+++ Scsd/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scsd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        public List<Dingdan> ls;
+        public List<Scpc> lspc;
+       // public List<Scpc> lspc2;
+       // public List<Scpc> lspc3;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            richTextBox1.Text = "";
+            ls = new List<Dingdan>();
+            ls.Add(new Dingdan(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), comboBox1.Text));
+            ls.Add(new Dingdan(Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text), comboBox2.Text));
+            ls.Add(new Dingdan(Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text), comboBox3.Text));
+            ls.Add(new Dingdan(Convert.ToInt32(textBox7.Text), Convert.ToInt32(textBox8.Text), comboBox4.Text));
+            ls.Sort();//排序
+
+
+            //去重
+            for (int i = 1; i < ls.Count; i++) {
+                if (ls[i - 1].tian == ls[i].tian && ls[i - 1].chanping == ls[i].chanping) {
+                    ls[i - 1].liang += ls[i].liang;
+                    ls.RemoveAt(i);
+                    i--;
+                }
+            }
+            
+
+            //去库存
+            int kucunA = Convert.ToInt32(textBox9.Text);
+            int kucunB = Convert.ToInt32(textBox11.Text);
+            int kucunC = Convert.ToInt32(textBox12.Text);
+            int shenA = kucunA;
+            int shenB = kucunB;
+            int shenC = kucunC;
+
+            for (int i=0; i < ls.Count; i++) {
+                if (ls[i].liang >= shenA && ls[i].chanping=="A") { ls[i].liang = ls[i].liang - shenA; break; }
+                else if(ls[i].chanping == "A") { shenA -= ls[i].liang; ls[i].liang = 0; }
+            }
+            for (int i = 0; i < ls.Count; i++)
+            {
+                if (ls[i].liang >= shenB && ls[i].chanping == "B") { ls[i].liang = ls[i].liang - shenB; break; }
+                else if (ls[i].chanping == "B") { shenB -= ls[i].liang; ls[i].liang = 0;  }
+            }
+            for (int i = 0; i < ls.Count; i++)
+            {
+                if (ls[i].liang >= shenC && ls[i].chanping == "C") { ls[i].liang = ls[i].liang - shenC; break; }
+                else if (ls[i].chanping == "C") { shenC -= ls[i].liang; ls[i].liang = 0;  }
+            }
+            
+            String ss = "\n";
+            foreach (Dingdan d in ls) {
+                ss += d.ToString() + "\n";
+            }
+            richTextBox1.Text += ss;
+
+            
+
+            lspc = new List<Scpc>();
+
+           
+            int pp = 0;
+            foreach (IGrouping<int, Dingdan> group in ls.GroupBy(c => c.tian))
+            {
+                int s, end;
+                s = pp;
+                end = group.Key;
+                pp = end;
+                List<TianAP> lsT = new List<TianAP>();
+                double zs = 0;
+                foreach (Dingdan d in group)
+                {
+                    double s1 = d.liang / Convert.ToDouble(end - s);
+                    lsT.Add(new TianAP(d.chanping,s1));
+                    zs += s1;
+                }
+                lspc.Add(new Scpc(s, end, zs, lsT));
+            }
+
+            int maxchan = Convert.ToInt32(textBox10.Text);
+
+            List<Scpc> lsp = you(lspc, maxchan, 0);
+            ss = "\n最优排程\n";
+            foreach (Scpc spc in lsp)
+            {
+                ss += spc.ToString() + "\n";
+            }
+            richTextBox1.Text += ss;
+
+
+            ss = "\n结果：";
+            bool flag = true;
+            foreach (Scpc spc in lsp)
+            {
+                if (spc.sudu > maxchan) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (!flag) ss += "新接的订单不能按时完成。请延缓订单，减少订货数量或增加生产线";
+            else  ss += "新接的订单能按时完成。";
+            richTextBox1.Text += ss;
+
+        }
+        public List<Scpc> you(List<Scpc> lss,int maxchan,int p) {
+            bool flag=false;
+            for (int i= 1;i< lss.Count; i++)
+            {
+                
+                if (lss[i].sudu > maxchan)
+                {
+                    double yu = (lss[i].end - lss[i].strart) * (lss[i].sudu - maxchan);
+                    lss[i].sudu = maxchan;
+                    lss[i - 1].sudu += yu / Convert.ToDouble(lss[i - 1].end - lss[i - 1].strart);
+                    for (int j =0; j < lss[i].tianls.Count; j++) {
+                        if (lss[i].tianls[j].sudu * (lss[i].end - lss[i].strart) >yu)
+                        {
+                            lss[i].tianls[j].sudu = lss[i].tianls[j].sudu - yu / Convert.ToDouble(lss[i].end - lss[i].strart);
+                            int ppp = -1;
+                            for (int h = 0; h < lss[i - 1].tianls.Count; h++)
+                            {
+                                if (lss[i - 1].tianls[h].chanping == lss[i].tianls[j].chanping) { ppp = h; break; }
+                            }
+                            if (ppp == -1) lss[i - 1].tianls.Add(new TianAP(lss[i].tianls[j].chanping, yu / Convert.ToDouble(lss[i-1].end - lss[i-1].strart)));
+                            else lss[i - 1].tianls[ppp].sudu += yu / Convert.ToDouble(lss[i-1].end - lss[i-1].strart);
+                            break;
+                        }
+                        else {
+                            
+                            int ppp = -1;
+                            for (int h = 0; h < lss[i - 1].tianls.Count; h++)
+                            {
+                                if (lss[i - 1].tianls[h].chanping == lss[i].tianls[j].chanping) { ppp = h; break; }
+                            }
+                            if (ppp == -1) lss[i - 1].tianls.Add(new TianAP(lss[i].tianls[j].chanping, lss[i].tianls[j].sudu * (lss[i].end - lss[i].strart) / Convert.ToDouble(lss[i - 1].end - lss[i - 1].strart)));
+                            else lss[i - 1].tianls[ppp].sudu += lss[i].tianls[j].sudu * (lss[i].end - lss[i].strart) / Convert.ToDouble(lss[i - 1].end - lss[i - 1].strart);
+                            yu -= lss[i].tianls[j].sudu * (lss[i].end - lss[i].strart);
+                            lss[i].tianls.RemoveAt(j);
+                            j--;
+                        }
+                        if (yu == 0) break;
+                    }                               
+                    flag = true;
+                    if(i==1) flag = false;
+                }
+                else flag = false;
+                if (i > p) p = i;
+                if (flag) break;
+            }
+            if (p == lss.Count - 1 && !flag) return lss;
+            else return you(lss, maxchan, p);
+        }
+        public class Dingdan : IComparable<Dingdan>
+        {
+            public int tian;
+            public int liang;
+            public String chanping;
+            public Dingdan(int t, int l,String c) {
+                this.tian = t;
+                this.liang = l;
+                this.chanping = c;
+            }
+            public int CompareTo(Dingdan other)
+            {
+                if (other == null)
+                    return 1;
+                int value = this.tian - other.tian;
+                if (value == 0)
+                    value = string.Compare(this.chanping, other.chanping);
+                return value;
+            }
+            public override string ToString()
+            {
+                return tian + "天后需生产"+chanping+"产品" + liang+"件";
+
+            }
+
+        }
+        public class Scpc
+        {
+            public int strart;
+            public int end;
+            public double sudu;
+            public List<TianAP> tianls;
+            public Scpc(int s, int e, double sd, List<TianAP> ls)
+            {
+                this.strart = s;
+                this.end = e;
+                this.sudu = sd;
+                this.tianls = ls;
+            }
+            public override string ToString()
+            {
+                String s= strart + "-" + end + "天，总生产速度为：" + sudu + "/天,明细如下：\n";
+                foreach(TianAP t in this.tianls) {
+                    s+= "     生产"+t.chanping+"商品，速度为："+t.sudu+ "/天\n";
+
+                }
+                return s;
+            }
+
+        }
+        public class TianAP{
+            public String chanping;
+            public double sudu;
+            public TianAP(string s ,double d) {
+                this.chanping = s;
+                this.sudu = d;
+            }
+        }
+
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox13_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox14_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
